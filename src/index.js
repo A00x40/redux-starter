d223a5412884cd8cd0daa640a7d2054e1aaef723
a00x40 <ahmedattia621@yahoo.com>
2021-09-02 17:51:35 +0200

    Added UI & Applied state changes to it

--- src/index.js
+++ src/index.js
@@ -1,10 +1,51 @@
 import store from './store'
 import * as actionCreators from './actionCreators'
 
+/** UI */
+const bugDescription = document.getElementById("bug")
+const bugAdd = document.getElementById("add-bug")
+
+const bugId = document.getElementById("bug-id")
+const bugRemove = document.getElementById("remove-bug")
+
+const bugList = document.getElementById("bug-list")
+const bugRender = () => {
+    if(store.getState().length !== 0) {
+        store.getState().map( bug => {
+            let listItem = document.getElementById(bug.id)
+            if(!listItem) {
+                var li = document.createElement("li");
+                var text = document.createTextNode(`Id: ${bug.id}, ` + `Descripion: ${bug.description}`);
+                li.appendChild(text)
+                li.id = bug.id
+                bugList.appendChild(li)
+            }
+        })
+    }
+}
+
+/** STORE SUBSCRIBE */
 const unsubscribe = store.subscribe( () => {
-    console.log("Store Changed " , store.getState())
+    alert("Store Changed")
+    console.log(store.getState())
+    bugRender()
 })
 
-store.dispatch( actionCreators.addBug("Bug1") )
+/** UI EVENTS */
+bugAdd.addEventListener( "click" , () => {
+    if(bugDescription.value !== "") {
+        store.dispatch(actionCreators.addBug(bugDescription.value))
+    } else {
+        alert("Please Fill the description")
+    }
+})
 
-store.dispatch( actionCreators.removeBug(1) )
+bugRemove.addEventListener( "click" , () => {
+    let id = bugId.value
+    var regex=/^[0-9]+$/;
+    if (id.match(regex)) {
+        store.dispatch(actionCreators.removeBug(parseInt(id)))
+    } else {
+        alert("Bug id must be a non negative number")
+    }
+})
